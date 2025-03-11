59092212bb72ff3fea52723379a914303a2a7f05
christian <christian.m@yourapps.co.ke>
2025-03-11 15:57:33 +0300

    Fix Linter Issues

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,12 @@
 #!/usr/bin/node
 
-if (process.argv.length < 3) { console.log('No argument'); } else { console.log(process.argv[2]); }
+const length = (array) => {
+  let len = 0;
+  while (true) {
+    if (array[len] === undefined) { break; }
+    len++;
+  }
+  return len;
+};
+
+if (length(process.argv) < 3) { console.log('No argument'); } else { console.log(process.argv[2]); }
