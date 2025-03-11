210573aaa90421304e78d2ce3b9656bb5d3ae0fe
christian <christian.m@yourapps.co.ke>
2025-03-11 16:44:25 +0300

    Complete task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
-function add(a, b) { return a + b; }
+function add (a, b) { if (isNaN(a) || isNaN(b)) {return NaN;} return a + b; }
 console.log(add(process.argv[2], process.argv[3]));
