d2ae7cd5b145f14eb95c60ad25a0470dbfbf9a07
christian <christian.m@yourapps.co.ke>
2025-03-11 16:24:42 +0300

    Fux Linter Issues

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,4 +2,4 @@
 
 let i = 0;
 if (!process.argv[2]) { console.log('Missing number of occurrences'); }
-while (i <= process.argv[2]) { console.log('C is fun'); i++; }
+while (i < process.argv[2]) { console.log('C is fun'); i++; }
