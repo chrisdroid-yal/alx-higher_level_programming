7d03b68bda41914171b790614f81603332e4bdbc
christian <christian.m@yourapps.co.ke>
2025-03-14 11:13:19 +0300

    Complete task 7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env
+
+const nbOccurences = (list, searchElement) => {
+  let i = 0; let count = 0;
+  while (i < list.length) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+    i++;
+  }
+  return count;
+};
+
+module.exports = { nbOccurences };
